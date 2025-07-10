6bf03cfa3dbd3de7fa897a4e0edcca7dc0314230
zy0n.bear <zy0n@users.noreply.github.com>
2025-07-09 22:21:25 -0400

    feat: export additional Poseidon modules for enhanced functionality

--- src/primatives/index.ts
+++ src/primatives/index.ts
@@ -3,3 +3,5 @@ export * as poseidonLib from 'poseidon-lite'
 export * from './utils'
 // export lite-wrapper functions, these are sync methods.
 export * from './poseidon/poseidon-lite'
+export * from './poseidon/poseidon-circomlibjs'
+export * from './poseidon/poseidon-module'
