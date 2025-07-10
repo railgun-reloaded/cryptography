1a9094b8917af82eae020d52e5024716e3c45787
zy0n.bear <zy0n@users.noreply.github.com>
2025-07-10 00:31:36 -0400

    refactor(eddsa): update buildEddsa function to accept injected poseidon instance

--- test/primatives/eddsa/index.test.ts
+++ test/primatives/eddsa/index.test.ts
@@ -4,24 +4,22 @@ import { describe, it } from 'node:test'
 
 import { bytesToHex } from '@noble/hashes/utils'
 
-import { autoInitializeEddsa, eddsa, initCircomlib, poseidonBuild } from '../../../src'
+import { autoInitializeEddsa, eddsa, initCircomlib } from '../../../src'
 
-describe('EDDSA module', () => {
+describe.only('EDDSA module', () => {
   it('Shold sign and verify signature', async () => {
     await initCircomlib('pure')
-    console.log(poseidonBuild)
     await autoInitializeEddsa()
     const privateKey = randomBytes(32)
     const pubKey = eddsa.privateKeyToPublicKey(privateKey)
     const message = randomBytes(32)
     eddsa.privateKeyToPublicKey(privateKey)
     const signature = eddsa.signPoseidon(privateKey, message)
-    // console.log('signature', signature)
     const verified = eddsa.verifyEDDSA(message, {
       R8: [signature[0], signature[1]],
       S: BigInt('0x' + bytesToHex(signature[2]))
     }, pubKey)
-    console.log('verified', verified)
-    assert(signature)
+    assert(signature, 'Signature not generated.')
+    assert(verified, 'Signature not verified.')
   })
 })
