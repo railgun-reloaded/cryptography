d5782f1657e4713d80efd600e8b38a959c9e019d
mattgle <44204622+mattgle@users.noreply.github.com>
2026-02-05 05:04:40 -0300

    Specify return type for poseidon function

--- src/primatives/poseidon/poseidon-circomlibjs.ts
+++ src/primatives/poseidon/poseidon-circomlibjs.ts
@@ -34,7 +34,7 @@ const initCircomlib = async (type: 'pure' | 'wasm') => {
  * @returns The computed Poseidon hash as a Uint8Array.
  * @throws Will throw an error if the Poseidon implementation has not been loaded.
  */
-const poseidon = (inputs: Uint8Array[]) => {
+const poseidon = (inputs: Uint8Array[]): Uint8Array => {
   // prefer wasm, (dev) must be manually initialized as such.
   const p = typeof typeof poseidonBuild.wasm === 'undefined' ? poseidonBuild.pure : poseidonBuild.wasm
   if (typeof p === 'undefined') {
